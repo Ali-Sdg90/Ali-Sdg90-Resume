d7016fdcaedb2afb3d1ea4e601f67baf26e98193
Ali Sadeghi <ali909392@gmail.com>
2026-07-19 23:50:39 +0330

    fix: write the first 10 gallery description

--- src/data/howItWasBuilt/howItWasBuiltData.js
+++ src/data/howItWasBuilt/howItWasBuiltData.js
@@ -62,7 +62,7 @@ export const howItWasBuiltData = {
             id: "history-01",
             translations: {
                 en: {
-                    title: "Initial Project Idea",
+                    title: "Shaping the First Vision",
                     description: `I always wanted to have a personal portfolio. A place where I could show the things I’ve built in one place, but not just as a simple normal page. I wanted the portfolio to fully feel like Ali. Something that made it clear from the start that it was built with my own creativity and personality.
 
 Something like a hub. A connection point for my projects, my resume, my social media links, and the things related to me.
@@ -74,7 +74,7 @@ I even thought it might be fun to have Ali on the side, waving in stop motion, f
 I drew the first version of the idea very simply in Paint, and this became the first initial design of the project.`,
                 },
                 fa: {
-                    title: "ایده اولیه پروژه",
+                    title: "رسیدن به ویژن اولیه",
                     description: `من همیشه دوست داشتم یه پورتفولیوی شخصی داشته باشم؛ جایی که بتونم ساخته‌هام رو یک‌جا به نمایش بذارم، ولی فقط یه صفحه‌ی ساده و معمولی نباشه. می‌خواستم پورتفولیو کاملا به مدل علی باشه. چیزی که از همون اول معلوم باشه با خلاقیت و شخصیت خودم ساخته شده.
 
 یه چیزی شبیه یه هاب؛ یه نقطه‌ی اتصال برای پروژه‌هام، رزومه‌م، لینک‌های سوشال مدیاهام و چیزهایی که به من مربوط می‌شن.
@@ -97,14 +97,32 @@ I drew the first version of the idea very simply in Paint, and this became the f
             id: "history-02",
             translations: {
                 en: {
-                    title: "Placeholder",
-                    description:
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    title: "Shaping the First Design",
+                    description: `I told ChatGPT about my idea, the vision I had for it, and how I wanted it to be built. I also gave it my resume, and we talked about the design of my portfolio together.
+
+In the end, we got to this image. It felt really special, creative, and exciting to me.
+
+A three-dimensional shelf with a nice perspective in the middle of the page, simple text and contact information on the right, and me in stop motion on the left.
+
+It had a small cyberpunk neon-light feeling. Not too loud, not too invisible.
+
+The design was something I was excited to build myself. I also had some experience creating objects with perspective, so I didn’t see it as completely out of my reach.
+
+So now that I had the design and the vision, I could start the project.`,
                 },
                 fa: {
-                    title: "متن جایگزین",
-                    description:
-                        "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است.",
+                    title: "رسیدن به دیزاین اولیه",
+                    description: `ایده‌م رو به چت‌جی‌پی‌تی گفتم، ویژنی که براش داشتم و می‌خواستم اونجوری ساخته بشه. همراه ایده‌م، رزومه‌م رو هم بهش دادم و با هم راجب دیزاین پورتفولیوم گپ زدیم.
+
+در نهایت به این عکس رسیدیم. چیزی بود که به نظرم خیلی خاص و خلاقانه و جذاب بود.
+
+شلف سه‌بعدی با پرسپکتیو خوشگل وسط صفحه، نوشته معمولی و اطلاعات تماس سمت راست و خودم به‌صورت استاپ‌موشن سمت چپ.
+
+یه حالت سایبرپانکی نئون‌لایتی ریز داشت. نه خیلی تو چشم، نه خیلی نامرئی.
+
+دیزاین چیزی بود که خودم برای ساختنش هیجان داشتم. تجربه‌ی ساخت اجسام با پرسپکتیو هم داشتم و کامل خارج از توانم نمی‌دیدم.
+
+پس حالا که دیزاین و ویژن رو داشتم، می‌تونستم پروژه رو شروع کنم.`,
                 },
             },
             image: {
@@ -118,14 +136,16 @@ I drew the first version of the idea very simply in Paint, and this became the f
             id: "history-03",
             translations: {
                 en: {
-                    title: "Placeholder",
-                    description:
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    title: "Shaping the Heart of the Project",
+                    description: `With ChatGPT image generation, I built the shelf. It took a lot of back and forth and changes to create something that matched the design and what I wanted, but in the end it gave me something that felt perfect to me.
+
+I brought the image into Photoshop and removed its background. I wanted the shadow and glowing reflection of the shelf to be visible on the floor, so I created a gradient over part of the ground that made the floor background color become transparent inside the gradient. That way, it looked like the shadow was actually on the floor, and it felt natural that the shelf was really placed there with its reflection on the ground.`,
                 },
                 fa: {
-                    title: "متن جایگزین",
-                    description:
-                        "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است.",
+                    title: "ساخت قلب پروژه",
+                    description: `با کمک image generation چت‌جی‌پی‌تی شلف رو ساختم. برای ساخت چیزی که توی دیزاین بود و خودم می‌خواستم، کلی بالا و پایین و تغییرات لازم بود، ولی در نهایت چیزی داد که به نظرم پرفکت بود.
+
+عکس رو توی فتوشاپ بردم و بک‌گراندش رو جدا کردم. می‌خواستم روی زمین، سایه و بازتاب درخشان شلف معلوم باشه، پس اومدم توی یه محوطه از روی زمین، یه حالت گرادینت ایجاد کردم که رنگ بک‌گراند زمین داخل گرادینت ترنسپرنت بشه و انگار واقعا این سایه روی زمین هست و به چشم طبیعی بیاد که شلف واقعا روی زمین قرار داره و بازتابش هم روی زمین هست.`,
                 },
             },
             image: {
@@ -139,14 +159,32 @@ I drew the first version of the idea very simply in Paint, and this became the f
             id: "history-04",
             translations: {
                 en: {
-                    title: "Placeholder",
-                    description:
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    title: "Placing the Heart of the Project",
+                    description: `First, I finalized the background animation so its color would change very slowly with a subtle flicker. Not too lifeless, not too distracting.
+
+Then I placed the shelf image in the middle of the page and tried to build the ground.
+
+I used a good dominant color for the ground so the colors of the floor, the image, and the background would blend together better, and it would not be easy to see where the image starts and where it ends.
+
+I also added a gradient between the ground and the background so the sharp edge between them would not distract the eye.
+
+Then I just placed the shelf items on the page.
+
+Now it was time to arrange them.`,
                 },
                 fa: {
-                    title: "متن جایگزین",
-                    description:
-                        "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است.",
+                    title: "قرار دادن قلب پروژه",
+                    description: `اول حالت انیمیشن بک‌گراند رو نهایی کردم که رنگش خیلی آروم و سوسو کنان تغییر بکنه. نه خیلی بی‌روح باشه، نه حواس‌پرت‌کن.
+
+بعد عکس شلف رو وسط صفحه گذاشتم و سعی کردم زمین رو بسازم.
+
+برای زمین از یه رنگ غالب خوب استفاده کردم تا رنگ‌های زمین، عکس و بک‌گراند بهتر با هم حل بشن و به‌راحتی معلوم نباشه عکس از کجا شروع می‌شه و کجا تموم می‌شه.
+
+یه گرادینت هم برای مرز زمین تا بک‌گراند دادم که تیزی مرزشون حواس رو پرت نکنه.
+
+بعد آیتم‌های شلف رو فقط روی صفحه گذاشتم.
+
+حالا نوبت چیدنشون بود.`,
                 },
             },
             image: {
@@ -160,14 +198,32 @@ I drew the first version of the idea very simply in Paint, and this became the f
             id: "history-05",
             translations: {
                 en: {
-                    title: "Placeholder",
-                    description:
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    title: "The First Fight With Perspective",
+                    description: `Placing the cards according to the perspective of the shelf image was a really complicated process...
+
+It was all about playing with the perspective origin, the perspective value, and the X, Y, and Z rotation angles of the cards...
+
+But in the end, I got to something that felt almost okay to me.
+
+I realized that each row unfortunately needs its own custom values.
+
+At this stage, I only used the Things I’ve Built row as my working reference and just tried to get its overall feel right.
+
+So later, I could go back and find the correct values for each shelf.`,
                 },
                 fa: {
-                    title: "متن جایگزین",
-                    description:
-                        "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است.",
+                    title: "اولین نبرد با پرسپکتیو",
+                    description: `قرار دادن کارت‌ها طبق پرسپکتیو عکس شلف، پروسه خیلی پیچیده‌ای بود...
+
+همش بازی با اوریجین پرسپکتیو، مقدار پرسپکتیو، زاویه‌های X و Y و Z چرخش کارت‌ها...
+
+ولی در نهایت به چیزی رسیدم که به نظرم تقریبا اوکی بود.
+
+فهمیدم هر سطر متاسفانه نیاز به مقادیر کاستوم خودش داره.
+
+تو این مرحله فعلا فقط ردیف Things I’ve Built رو الگوی کارم قرار دادم و فقط سعی کردم کلیت اون رو درست کنم.
+
+تا بعدا برم و برای هر شلف مقدار درست به دست بیارم.`,
                 },
             },
             image: {
@@ -181,14 +237,28 @@ I drew the first version of the idea very simply in Paint, and this became the f
             id: "history-06",
             translations: {
                 en: {
-                    title: "Placeholder",
-                    description:
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    title: "Taming the Angles and Motion",
+                    description: `With a lot of precision, I managed to get the card angles right!
+
+I kept telling myself that if I could not get the angles right, it would be the first thing people would notice unconsciously at first glance, and it could make all the work I had done, and all the work ahead of me, feel worthless. If it looked off, the user would feel that something here is wrong. It does not feel natural.
+
+I also realized that the height of the cards in each row needed to be different too.
+
+Then I added the infinite card behavior, the acceleration and natural dragging behavior of the cards, and the shadow at the start and end of each row.
+
+The movement speed of the cards and the amount of friction when they were thrown felt natural and fun to me.`,
                 },
                 fa: {
-                    title: "متن جایگزین",
-                    description:
-                        "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است.",
+                    title: "رام کردن زاویه‌ها و حرکت",
+                    description: `با ظرافت خیلی زیاد تونستم زاویه‌ی کارت‌ها رو درست دربیارم!
+
+با خودم می‌گفتم اگه نتونم زاویه‌ها رو درست دربیارم، اولین چیزی هست که به‌صورت ناخودآگاه توی دید اول به چشم میاد و می‌تونه کل زحمات الان و آینده‌م رو بی‌ارزش کنه. اگه مشکل‌دار باشه، کاربر می‌گه اینجا یه چیزی اشتباهه. طبیعی نیست.
+
+و فهمیدم ارتفاع کارت‌های ردیف‌ها هم لازمه متفاوت باشه.
+
+بعد قابلیت بی‌نهایت بودن کارت‌ها، شتاب و رفتار طبیعی کشیدن کارت‌ها، و سایه‌ی شروع و پایان ردیف رو اضافه کردم.
+
+سرعت حرکت کارت‌ها و مقدار اصطکاک پرت شدن کارت‌ها، به نظرم طبیعی و فان شده بود.`,
                 },
             },
             image: {
@@ -202,14 +272,20 @@ I drew the first version of the idea very simply in Paint, and this became the f
             id: "history-07",
             translations: {
                 en: {
-                    title: "Placeholder",
-                    description:
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    title: "Shaping the Shelf's DNA",
+                    description: `I also made the card width separate data for each row. Now the length, width, angle, perspective, speed, behavior, and data of each row were unique to that row, and were passed into the template file to be built.
+
+I finalized the short descriptions of my projects, added square placeholders for the images, and improved the style and spacing of the cards.
+
+For the first time, I could really see how beautiful it was going to become!`,
                 },
                 fa: {
-                    title: "متن جایگزین",
-                    description:
-                        "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است.",
+                    title: "شکل دادن DNA شلف",
+                    description: `عرض کارت‌ها هم دیتای جدا برای هر ردیف کردم. الان طول، عرض، زاویه، پرسپکتیو، سرعت، رفتار و دیتای هر ردیف، مختص به خودش بود و برای ساخته شدن، به فایل تمپلیت داده می‌شد.
+
+دیسکریپشن کوتاه پروژه‌هام رو نهایی کردم، برای عکس‌ها مربع پلیس‌هولدر گذاشتم و استایل و فاصله‌گذاری کارت‌ها رو بهتر کردم.
+
+الان برای اولین بار می‌شد واقعا دید که چه چیز خوشگلی خواهد شد!`,
                 },
             },
             image: {
@@ -223,14 +299,28 @@ I drew the first version of the idea very simply in Paint, and this became the f
             id: "history-08",
             translations: {
                 en: {
-                    title: "Placeholder",
-                    description:
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    title: "Shaping the Project's Soul",
+                    description: `I added the images for the Tech Stack and Career sections.
+
+All of the images needed a quick visit to Photoshop so their size would be exactly 300 by 300, and so the logos would not stick too close to the border. I wanted them to have a little bit of space and sit nicely inside the cards. (This gets much better later.)
+
+Then I changed the image formats to jpg, kept their sizes roughly around 4 to 9 kilobytes, and compressed them properly.
+
+Now my project was not just a single blue color anymore. It felt like it had gained a soul, like it had earned badges of honor :)
+
+And I do not know why this screenshot is so much brighter than the others... but it turned out cool :)`,
                 },
                 fa: {
-                    title: "متن جایگزین",
-                    description:
-                        "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است.",
+                    title: "شکل دادن روح پروژه",
+                    description: `عکس‌های قسمت Tech Stack و Career رو گذاشتم.
+
+عکس‌ها همشون نیاز بود یه بار سر به فتوشاپ بزنن که اندازه‌شون حتما ۳۰۰ در ۳۰۰ بشه و تا جای ممکن لوگو خیلی به بوردر نچسبه و یکم فاصله داشته باشه که خوشگل تو کارت بشینه. (بعدا این خیلی بهترتر می‌شه)
+
+بعد پسوند عکس‌ها رو jpg کردم و حجمشون رو تو رنج تقریبی ۴ تا ۹ کیلوبایت نگه داشتم و حسابی کامپرسشون کردم.
+
+الان پروژه‌م دیگه فقط یه تک‌رنگ آبی‌ نبود. انگار روح گرفته بود، بج‌های آنر گرفته بود :)
+
+و نمی‌دونم چرا این اسکرین‌شات این‌قدر روشن‌تر از بقیه‌ست... ولی باحال شده :)`,
                 },
             },
             image: {
@@ -244,14 +334,24 @@ I drew the first version of the idea very simply in Paint, and this became the f
             id: "history-09",
             translations: {
                 en: {
-                    title: "Placeholder",
-                    description:
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    title: "Wingardium Leviosa",
+                    description: `This was where the most complicated change in the project happened. It is not really visible in the design, but I made all the items inside the shelf use cqw!
+
+Basically, the size of every item inside the shelf became dependent on the size of the shelf parent. This helped me finally implement a feature I was really stressed about, in a very clean way. The project would always look the way it was designed, on any screen, monitor, or zoom level.
+
+Then I made the size of the shelf parent div depend on the width and height of the monitor, so changing the zoom or the screen could no longer break the shelf, or make it and its items too small or too big!
+
+Oh, and yeah, I also filled the left side of the page.`,
                 },
                 fa: {
-                    title: "متن جایگزین",
-                    description:
-                        "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است.",
+                    title: "Wingardium Leviosa",
+                    description: `اینجا پیچیده‌ترین تغییر پروژه رخ داد. این تغییر تو ظاهر قابل‌تشخیص نیست. کاری کردم تمامی آیتم‌های داخل شلف، همشون از cqw استفاده کنن!
+
+یعنی اندازه‌ی تمامی آیتم‌های داخل شلف وابسته به اندازه‌ی پرنت شلف بود. این باعث شد بتونم فیچری که خیلی براش استرس داشتم رو بالاخره خیلی تمیز پیاده کنم. اینکه پروژه تو هر صفحه، مانیتور و زومی، همیشه اون‌جور که طراحی شده نشون داده بشه.
+
+بعد اندازه‌ی div پرنت شلف رو وابسته به عرض و ارتفاع مانیتور کردم و دیگه تغییر زوم و مانیتور نمی‌تونست باعث خراب شدن، خیلی کوچیک شدن یا خیلی بزرگ شدن شلف یا آیتم‌هاش بشه!
+
+آهان و آره، قسمت چپ صفحه هم پر کردم.`,
                 },
             },
             image: {
